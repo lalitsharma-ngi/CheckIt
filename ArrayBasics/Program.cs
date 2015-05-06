6c0214b0c418b88025028c2676e12f8521ec99e9
Lalit <lalit.sharma@ngiventures.com>
2015-05-06 23:45:05 +0530

    JustCHeck

---
+++ ArrayBasics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace ArrayBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'a';
+            a += 'c';
+            Console.WriteLine("Value of addition is={0}", a);
+            //string[] names = {"Apple", "Orange", "Grape","Banana"};
+            //IEnumerator e = names.GetEnumerator();
+            //while (e.MoveNext())
+            //{
+            //    string str = (string)e.Current;
+            //    Console.WriteLine("Fruit Name={0}",str);
+            //}
+            Console.WriteLine("Press any key to finish");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArrayBasics/StringPrg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayBasics
+{
+    class StringPrg
+    {
+        //public static void Main()
+        //{
+        //    string str = "HI Lalit kumar sharma";
+
+        //    string checkstring = "Lalit";
+
+
+
+
+        //}
+    }
+}
